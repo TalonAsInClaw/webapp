b4c5344c33295bc372a6a696962c0c949d9bd8d7
Troy Candelaria <38091014+TroyCandelaria@users.noreply.github.com>
2018-11-04 21:26:27 -0600

    Add files via upload

---
+++ webApplication/WebContent/Quote.js
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+class Quote {
+	constructor(clientId, gallonsRequested, deliveryDate, requestDate, 
+				deliveryLocation, deliveryContactName, deliveryContactPhone, 
+				deliveryContactEmail, suggestedPrice, totalAmountDue){
+		this.clientId = clientId;
+		this.gallonsRequested = gallonsRequested;
+		this.deliveryDate = deliveryDate;
+		this.requestDate = requestDate;
+		this.deliveryLocation = deliveryLocation;
+		this.deliveryContactName = deliveryContactName;
+		this.deliveryContactPhone = deliveryContactPhone;
+		this.deliveryContactEmail = deliveryContactEmail;
+		this.suggestedPrice = suggestedPrice;
+		this.totalAmountDue = totalAmountDue;
+	}
+	
+}
+
---
+++ webApplication/src/webApplication/QuoteHistory.java
@@ -0,0 +1,8 @@
+package webApplication;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QuoteHistory {
+	public List <Quote> Quotehistory = new ArrayList<Quote>();
+}
